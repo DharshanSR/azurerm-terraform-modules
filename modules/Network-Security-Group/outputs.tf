104a32401cdaf01473ecc4bcf87473ad60124336
DharshanSR <dharshanravindran8@gmail.com>
2025-10-31 22:56:59 +0530

    fix: Correct references to Network Security Group resource in outputs

--- modules/Network-Security-Group/outputs.tf
+++ modules/Network-Security-Group/outputs.tf
@@ -1,14 +1,14 @@
 output "id" {
   description = "The ID of the Network Security Group"
-  value       = azurerm_network_security_group.nsg.id
+  value       = azurerm_network_security_group.network_security_group.id
 }
 
 output "name" {
   description = "The name of the Network Security Group"
-  value       = azurerm_network_security_group.nsg.name
+  value       = azurerm_network_security_group.network_security_group.name
 }
 
 output "security_rules" {
   description = "The security rules of the Network Security Group"
-  value       = azurerm_network_security_group.nsg.security_rule
+  value       = azurerm_network_security_group.network_security_group.security_rule
 }
