051a09db3ecc2e3f6431fc5717868033aa59666e
Ravindran Dharshan <dharshanravindran8@gmail.com>
2025-10-31 17:52:42 +0530

    refactor: Remove unused subnet network security group association resource

--- modules/Network-Security-Group/main.tf
+++ modules/Network-Security-Group/main.tf
@@ -4,8 +4,3 @@ resource "azurerm_network_security_group" "network_security_group" {
   resource_group_name = var.resource_group_name
   tags                = var.tags
 }
-
-resource "azurerm_subnet_network_security_group_association" "network_security_group_association" {
-  subnet_id                 = azurerm_subnet.subnet.id
-  network_security_group_id = azurerm_network_security_group.network_security_group.id
-}
